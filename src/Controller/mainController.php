b68abdd53696d5a6ae7c88dc2abb9441b89543c6
m <m@gmail.com>
2021-02-02 13:24:07 +0100

    add profiler + create service request coinbase + add httpclient

--- src/Controller/mainController.php
+++ src/Controller/mainController.php
@@ -6,13 +6,25 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Service\CoinbaseClient;
+
+/**
+ * @property CoinbaseClient requestclient
+ */
 
 class mainController extends AbstractController
 {
+
+    public function __construct(CoinbaseClient $requestClient)
+    {
+        $this->requestclient = $requestClient;
+    }
+
     /**
-     * @Route("/", name="indexRoute")
+     * @Route("/accueil", name="indexRoute")
      */
     public function index(){
+        $this->requestclient->connexionWithApiKey();
         return $this->render('index.html.twig');
     }
 }
---
+++ src/Service/CoinbaseClient.php
@@ -0,0 +1,16 @@
+<?php
+
+
+namespace App\Service;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
+
+
+class CoinbaseClient extends AbstractController
+{
+    public function connexionWithApiKey(){
+        dump(2423);
+        die;
+    }
+}
